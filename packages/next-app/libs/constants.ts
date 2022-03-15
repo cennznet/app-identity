8276c1efb135bc0ccb73a16d6c96e060cef46123
aidan-starke <starke.aidan@gmail.com>
2022-03-15 14:02:35 +1300

    remove NEXT_PUBLIC_ from .env vars

--- packages/next-app/libs/constants.ts
+++ packages/next-app/libs/constants.ts
@@ -1,11 +1,11 @@
-export const NEXTAUTH_SECRET = String(process.env.NEXT_PUBLIC_NEXTAUTH_SECRET);
+export const NEXTAUTH_SECRET = String(process.env.NEXTAUTH_SECRET);
 
-export const TWITTER_ID = String(process.env.NEXT_PUBLIC_TWITTER_ID);
+export const TWITTER_ID = String(process.env.TWITTER_ID);
 
-export const TWITTER_SECRET = String(process.env.NEXT_PUBLIC_TWITTER_SECRET);
+export const TWITTER_SECRET = String(process.env.TWITTER_SECRET);
 
-export const DISCORD_ID = String(process.env.NEXT_PUBLIC_DISCORD_ID);
+export const DISCORD_ID = String(process.env.DISCORD_ID);
 
-export const DISCORD_SECRET = String(process.env.NEXT_PUBLIC_DISCORD_SECRET);
+export const DISCORD_SECRET = String(process.env.DISCORD_SECRET);
 
 export const REDIS_URL = String(process.env.REDIS_URL);

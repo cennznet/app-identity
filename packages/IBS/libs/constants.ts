0bcdae5a3496e8dbad65ce7eb850424acc24ca3c
aidan-starke <starke.aidan@gmail.com>
2022-03-18 10:41:53 +1300

    make use of ts paths, add `constants.ts`

---
+++ packages/IBS/libs/constants.ts
@@ -0,0 +1,5 @@
+export const CENNZ_PROVIDER: string = String(process.env.CENNZ_PROVIDER);
+
+export const REG_INDEX: number = Number(process.env.REG_INDEX);
+
+export const MONGO_DB_PASSWORD: string = String(process.env.MONGO_DB_PASSWORD);

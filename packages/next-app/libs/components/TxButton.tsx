2ee833c76c517a0d123542a5a3abd6d6c47e1cc6
aidan-starke <starke.aidan@gmail.com>
2022-03-19 08:25:42 +1300

    add hover effects if clickable, `cursor: not-allowed` if not

--- packages/next-app/libs/components/TxButton.tsx
+++ packages/next-app/libs/components/TxButton.tsx
@@ -47,7 +47,7 @@ const TxButton: FC<{
 	}
 
 	return (
-		<div css={styles.button(null, true)}>
+		<div css={styles.notButton}>
 			<p>PLEASE SIGN IN WITH A VALID ACCOUNT</p>
 		</div>
 	);
@@ -84,7 +84,41 @@ export const styles = {
 						font-size: 10px;
 					}
 				}
+
+				&:hover {
+					background-color: white;
+					border: 1px solid
+						${cennznet ? palette.primary.main : palette.primary[authProvider]};
+					color: ${cennznet
+						? palette.primary.main
+						: palette.primary[authProvider]};
+					transition-duration: 0.3s;
+					text-shadow: none;
+				}
 			`,
+	notButton: ({ palette }) => css`
+		cursor: not-allowed;
+		width: 90%;
+		height: 40px;
+		margin: 1em auto;
+		text-align: center;
+		border-radius: 5px;
+		background-color: ${palette.primary.main};
+		color: white;
+		letter-spacing: 0.5px;
+		justify-content: center;
+		align-items: center;
+		display: flex;
+		font-weight: bold;
+		text-shadow: 2px 2px rgba(0, 0, 0, 0.15);
+
+		p {
+			font-size: 14px;
+			@media (max-width: 500px) {
+				font-size: 10px;
+			}
+		}
+	`,
 	changeAccount: ({ palette }) => css`
 		margin: 0 auto;
 		width: auto;
